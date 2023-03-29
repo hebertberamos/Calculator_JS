1ba2e986f9a0ef77b86c8557d173497e826292c4
hebert benigno <hebertramos09@gmail.com>
2023-03-29 14:00:54 -0300

    adding clear functionality to two buttons, one to clear only the number that is in the input and the other to clear everything

--- js/calculator-controller.js
+++ js/calculator-controller.js
@@ -7,8 +7,13 @@ function State(){
     this.Numbers = new Numbers();
     this.Expression = new Expression();
 
+    this.num1 = null;
+    this.num2 = null;
+    this.exp = null;
+
     this.input = null;
 
+    this.btnClearAll = null;
     this.btnClear = null;
     this.btnTurnProcentage = null;
 
@@ -34,8 +39,14 @@ function State(){
 const state = new State();
 
 export function init(){
+    state.num1 = document.querySelector("#num1");
+    state.num2 = document.querySelector("#num2");
+    state.exp = document.querySelector("#exp");
+    
     state.input = document.querySelector("#input");
-    state.btnClear = document.querySelector("#clearAll");
+
+    state.btnClearAll = document.querySelector("#clearAll");
+    state.btnClear = document.querySelector("#clear");
     state.btnTurnProcentage = document.querySelector("#turnPorcentage");
     state.btnDivide = document.querySelector("#divide");
     state.btnMultiply = document.querySelector("#multiply");
@@ -54,74 +65,62 @@ export function init(){
     state.btnEight = document.querySelector("#numberEight");
     state.btnNine = document.querySelector("#numberNine");
 
-    state.input.addEventListener('change', handleInputChange);
-
     state.btnZero.addEventListener('click', (event) => {
         event.preventDefault();
         displayValue1 += 0;
-        resetNumberClick();
         changeDisplay(displayValue1);
     });
 
     state.btnOne.addEventListener('click', (event) => {
         event.preventDefault();
         displayValue1 += 1;
-        resetNumberClick();
         changeDisplay(displayValue1);
     });
 
     state.btnTwo.addEventListener('click', (event) => {
         event.preventDefault();
         displayValue1 += 2;
-        resetNumberClick();
         changeDisplay(displayValue1);
     });
 
     state.btnThree.addEventListener('click', (event) => {
         event.preventDefault();
         displayValue1 += 3;
-        resetNumberClick();
         changeDisplay(displayValue1);
     });
 
     state.btnFour.addEventListener('click', (event) => {
         event.preventDefault();
         displayValue1 += 4;
-        resetNumberClick();
         changeDisplay(displayValue1);
     })
 
     state.btnFive.addEventListener('click', (event) => {
         event.preventDefault();
         displayValue1 += 5;
-        resetNumberClick();
         changeDisplay(displayValue1);
     });
 
     state.btnSix.addEventListener('click', (event) => {
         event.preventDefault();
         displayValue1 += 6;
-        resetNumberClick();
         changeDisplay(displayValue1);
     });
 
     state.btnSeven.addEventListener('click', (event) => {
         event.preventDefault();
-        resetNumberClick();
         displayValue1 += 7;
         changeDisplay(displayValue1);
     });
 
     state.btnEight.addEventListener('click', (event) => {
         event.preventDefault();
-        resetNumberClick();
         displayValue1 += 8;
         changeDisplay(displayValue1);
     });
 
     state.btnNine.addEventListener('click', (event) => {
         event.preventDefault();
-        resetNumberClick();
         displayValue1 += 9;
         changeDisplay(displayValue1);
     });
@@ -129,43 +128,48 @@ export function init(){
 
     state.btnSum.addEventListener('click', (event) => {
         event.preventDefault();
-        resetNumberClick();
-        handleInputChange(event);
+        handleInputChange();
         state.Expression.usedExpression = "+";
-        exp = "+";
+        state.exp.textContent = "+";
+        console.log("primeiro números: ", state.Numbers.number1);
+        console.log(state.Expression.usedExpression);
+        console.log("segundo números:", state.Numbers.number2);
+        if(state.Expression.usedExpression == "+" && !state.Numbers.number2 == ""){
+            const result = expressionController.sum(state.Numbers.number1, state.Numbers.number2);
+            state.Numbers.number1 = result;
+        }
+        if(!state.Numbers.number1 == "" && !state.Numbers.number2 == ""){
+            state.num1.textContent = state.Numbers.number1;
+            console.log("não estão vazios");
+            state.num2.textContent = "";
+        }
         clear();
+        state.input.textContent = state.Numbers.number1;
     });
 
     state.btnSubtract.addEventListener('click', (event) => {
         event.preventDefault();
-        resetNumberClick();
-        handleInputChange(event);
+        handleInputChange();
         state.Expression.usedExpression = "-";
-        exp = "-";
         clear();
     });
 
     state.btnMultiply.addEventListener('click', (event) => {
         event.preventDefault();
-        resetNumberClick();
-        handleInputChange(event);
+        handleInputChange();
         state.Expression.usedExpression = "*";
-        exp = "*";
         clear();
     });
 
     state.btnDivide.addEventListener('click', (event) => {sum
         event.preventDefault();
-        resetNumberClick();
-        handleInputChange(event);
+        handleInputChange();
         state.Expression.usedExpression = "/";
-        exp = "/";
         clear();
     });
 
     state.btnTurnProcentage.addEventListener('click', (event) => {
         event.preventDefault();
-        resetNumberClick();
         const result = expressionController.convertDecimal(state.input.textContent);
         state.Numbers.number1 = result;
         state.input.textContent = result;
@@ -173,8 +177,7 @@ export function init(){
 
     state.btnEquality.addEventListener('click', (event) => {
         event.preventDefault();
-        resetNumberClick();
-        handleInputChange(event);
+        handleInputChange();
 
         switch(state.Expression.usedExpression){
             case "+":
@@ -201,36 +204,38 @@ export function init(){
 
 
 
-    state.btnClear.addEventListener('click', (event) => {
+    state.btnClearAll.addEventListener('click', (event) => {
         event.preventDefault();
-        numClick++;
-        clickCount(numClick);
-        state.input.textContent = 0;
-        displayValue1 = "";
+        clearAll();
+    });
+
+    state.btnClear.addEventListener('click', (event) =>{
+        event.preventDefault();
+        clear();
+
     });
 
     state.btnComma.addEventListener('click', (event) => {
         event.preventDefault();
-        resetNumberClick();
         displayValue1 += ".";
         changeDisplay(displayValue1);
-    })
+    });
 }
 
-function handleInputChange(event){
+function handleInputChange(){
     if(!state.Numbers.number1 || state.Numbers.number1 == ""){
         state.Numbers.number1 = displayValue1;
+        state.num1.textContent = state.Numbers.number1;
     } 
     else{
         state.Numbers.number2 = displayValue1;
+        state.num2.textContent = state.Numbers.number2;
     }
 }
 
 //Funções secundárias
 
-let numClick = 0;
 let displayValue1 = "";
-let exp = "";
 
 function changeDisplay(value){
     state.input.textContent = value;
@@ -241,17 +246,12 @@ function clear(){
     displayValue1 = "";
 }
 
-
-function clickCount(value){
-    if(value >= 2){
-        state.Numbers.number1 = "";
-        state.Numbers.number2 = "";
-    }
-    else{
-        state.input.textContent = state.Numbers.number1;
-    }
-}
-
-function resetNumberClick(){
-    numClick = 0;
+function clearAll(){
+    state.num1.textContent = "";
+    state.num2.textContent = "";
+    state.exp.textContent = "";
+    state.input.textContent = 0;
+    displayValue1 = "";
+    state.Numbers.number1 = "";
+    state.Numbers.number2 = "";
 }
